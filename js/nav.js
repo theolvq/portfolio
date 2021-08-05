2cf44a6c7c4b51400ec2356250d4eb3acd271b58
daawascript <theo.leveque@gmail.com>
2021-08-04 22:57:50 -0700

    added animate.css

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,26 @@
-const hamburger = document.querySelector('#hamburger');
-const nav = document.querySelector('#nav');
+const toggleNav = (() => {
+  const hamburger = document.querySelector('#hamburger');
+  const nav = document.querySelector('#nav');
+  const toggle = () => {
+    console.log(nav.classList);
+    if (nav.classList.contains('nav--hidden')) {
+      nav.classList.remove('nav--hidden');
+    }
+    if (nav.classList.contains('animate__fadeInLeft')) {
+      nav.classList.add('animate__fadeOutLeft');
+      nav.classList.remove('animate__fadeInLeft');
+    }
+    if (nav.classList.contains('animate__fadeOutLeft')) {
+      nav.classList.add('animate__fadeInLeft');
+      nav.classList.remove('animate__fadeOutLeft');
+    }
+  };
 
-const toggleNav = () => {
-  nav.classList.toggle('nav--hidden');
-};
+  hamburger.addEventListener('click', toggle);
 
-hamburger.addEventListener('click', toggleNav);
+  return {
+    toggle,
+  };
+})();
 
 export default toggleNav;
