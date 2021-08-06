18f0d8028157765c3ee7fee50d5a443d8fe456cd
daawascript <theo.leveque@gmail.com>
2021-08-05 23:06:02 -0700

    added tsparticles and got basic pink and cyan particles floating

--- js/nav.js
+++ js/nav.js
@@ -2,18 +2,16 @@ const toggleNav = (() => {
   const hamburger = document.querySelector('#hamburger');
   const nav = document.querySelector('#nav');
   const toggle = () => {
-    console.log(nav.classList);
     if (nav.classList.contains('nav--hidden')) {
       nav.classList.remove('nav--hidden');
     }
-    if (nav.classList.contains('animate__fadeInLeft')) {
-      nav.classList.add('animate__fadeOutLeft');
-      nav.classList.remove('animate__fadeInLeft');
+    if (hamburger.textContent.trim() == 'menu') {
+      hamburger.textContent = 'close';
+    } else if (hamburger.textContent.trim() === 'close') {
+      hamburger.textContent = 'menu';
     }
-    if (nav.classList.contains('animate__fadeOutLeft')) {
-      nav.classList.add('animate__fadeInLeft');
-      nav.classList.remove('animate__fadeOutLeft');
-    }
+    nav.classList.toggle('animate__fadeOutLeft');
+    nav.classList.toggle('animate__fadeInLeft');
   };
 
   hamburger.addEventListener('click', toggle);
--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import './styles/base.css';
 import './styles/style.css';
 import '@csstools/normalize.css';
+import 'animate.css';
+import { tsParticles } from 'tsparticles';
 import toggleNav from './js/nav';
+
+tsParticles.loadJSON('tsparticles', 'particles.json').then((container) => {});
