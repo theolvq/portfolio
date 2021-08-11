4ce477e7633623fff3c104443de6f4ef0aed3a35
daawascript <theo.leveque@gmail.com>
2021-08-11 15:44:04 -0700

    added cards, content  and images for projects, styled using grid

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import './styles/styles.scss';
-import './styles/base.scss';
-import '@csstools/normalize.css';
+// import './styles/base.scss';
+// import '@csstools/normalize.css';
 import 'animate.css';
 import { tsParticles } from 'tsparticles';
 import toggleNav from './js/nav';
-import scroll100Vh from './js/scroll';
+// import scroll100Vh from './js/scroll';
 
 tsParticles.loadJSON('tsparticles', 'particles.json').then((container) => {});
