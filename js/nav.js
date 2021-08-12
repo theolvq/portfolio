65593e98d0a9a44a54a8e4bc425565540b8eb3aa
daawascript <theo.leveque@gmail.com>
2021-08-11 21:20:08 -0700

    added blur on card bg, moved nav from right to left

--- js/nav.js
+++ js/nav.js
@@ -3,14 +3,14 @@ const toggleNav = (() => {
   const nav = document.querySelector('#nav');
   const toggle = () => {
     if (hamburger.textContent.trim() == 'menu') {
-      nav.classList.add('animate__fadeInRight');
-      nav.classList.remove('animate__fadeOutRight');
+      nav.classList.add('animate__fadeInLeft');
+      nav.classList.remove('animate__fadeOutLeft');
       hamburger.textContent = 'close';
       nav.style.visibility = 'visible';
       nav.style.opacity = 1;
     } else if (hamburger.textContent.trim() === 'close') {
-      nav.classList.add('animate__fadeOutRight');
-      nav.classList.remove('animate__fadeInRight');
+      nav.classList.add('animate__fadeOutLeft');
+      nav.classList.remove('animate__fadeInLeft');
       hamburger.textContent = 'menu';
       nav.style.visibility = 'hidden';
       nav.style.opacity = 0;
--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 import './styles/styles.scss';
-// import './styles/base.scss';
-// import '@csstools/normalize.css';
+import '@csstools/normalize.css';
 import 'animate.css';
 import { tsParticles } from 'tsparticles';
 import toggleNav from './js/nav';
 // import scroll100Vh from './js/scroll';
 
-tsParticles.loadJSON('tsparticles', 'particles.json').then((container) => {});
+tsParticles.loadJSON('tsparticles', 'particles.json');
