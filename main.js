2857cb4f58d2d06c4d55b3c0541528301ef2c09d
daawascript <theo.leveque@gmail.com>
2021-08-24 21:20:02 -0700

    fixed linkedin broken link, adjusted css for projects section, remove 100vh scrolling

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-import './styles/styles.scss';
-import '@csstools/normalize.css';
-import 'animate.css';
-import toggleNav from './js/nav';
-import particles from './js/particles';
-import date from './js/date';
-import scroll100Vh from './js/scroll';
+import "./styles/styles.scss";
+import "@csstools/normalize.css";
+import "animate.css";
+import toggleNav from "./js/nav";
+import particles from "./js/particles";
+import date from "./js/date";
